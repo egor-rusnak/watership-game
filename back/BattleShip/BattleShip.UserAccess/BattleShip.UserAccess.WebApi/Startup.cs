b4fa30db2ff0ac15da7bdcf59c1f735dcfbedbb4
egor-rusnak <yehor.rusnak9982@gmail.com>
2022-02-06 00:48:59 +0200

    Created solution and folder for UserAccess

---
+++ back/BattleShip/BattleShip.UserAccess/BattleShip.UserAccess.WebApi/Startup.cs
@@ -0,0 +1,63 @@
+﻿namespace BattleShip.UserAccess.WebApi
+{
+    public class Startup
+    {
+        private IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration _, IHostEnvironment env)
+        {
+            var configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", reloadOnChange: true, optional: true)
+                .AddEnvironmentVariables();
+
+            if (env.IsDevelopment())
+            {
+                configurationBuilder.AddUserSecrets<Startup>(optional: true);
+            }
+
+            Configuration = configurationBuilder.Build();
+        }
+
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen();
+
+            services.AddControllers();
+
+            services.AddRouting(options => options.LowercaseUrls = true);
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AnyOrigin", x => x
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+        }
+
+        public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseCors(builder =>
+                builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
